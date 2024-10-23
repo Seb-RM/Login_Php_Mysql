74e94110f3067b5944b1ca35b909300efdcb0da2
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-10-23 13:34:29 -0300

    creación del formulario de login

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Formulario de Login</title>
+    </head>
+    <body>
+        <div class="principal">
+            <div class="formulario">
+                <div class="subtitulo">
+                    <h2>Login</h2>
+                </div>
+                <form action="" method="post">
+                    <label for="usuario">Usuario: </label>
+                    <br>
+                    <input type="text" name="usuario" id="">
+                    <br><br>
+                    <label for="contrasena">Contraseña</label>
+                    <br>
+                    <input type="password" name="contrasena" id="">
+                    <br>
+                    <button>Ingresar</button>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
