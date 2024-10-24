3ce0a55a0ea829f03aeacb8d34447d0a69e99102
Seb-RM <122194477+Seb-RM@users.noreply.github.com>
2024-10-23 22:04:05 -0300

    definicion de estilos css

--- login.php
+++ login.php
@@ -3,22 +3,21 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="login.css">
         <title>Formulario de Login</title>
     </head>
     <body>
         <div class="principal">
             <div class="formulario">
-                <div class="subtitulo">
-                    <h2>Login</h2>
-                </div>
                 <form action="" method="post">
-                    <label for="usuario">Usuario: </label>
-                    <br>
-                    <input type="text" name="usuario" id="">
+                    <div class="subtitulo">
+                        <h2>Login</h2>
+                    </div>
+                    <label for="usuario" class="LUsuario">Usuario: </label>
+                    <label for="contrasena" class="LContrasena">Contraseña:</label>
                     <br><br>
-                    <label for="contrasena">Contraseña</label>
-                    <br>
-                    <input type="password" name="contrasena" id="">
+                    <input type="text" name="usuario" id="" class="IUsuario">
+                    <input type="password" name="contrasena" id="" class="IContrasena">
                     <br>
                     <button>Ingresar</button>
                 </form>
